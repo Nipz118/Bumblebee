a40cfce30a4ecbaa7cb435c8876abebbf99b793b
Nipuni Perera <93609296+Nipz118@users.noreply.github.com>
2023-04-09 16:24:36 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import Pagenotfound from './pages/Pagenotfound';
+import Register from './pages/Auth/Register';
+import 'react-toastify/dist/ReactToastify.css';
+import Login from './pages/Auth/Login';
+import Dashboard from './pages/user/Dashboard';
+import PrivateRoute from './components/Routes/Private';
+import AdminRoute from './components/Routes/AdminRoute';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import CreateCategory from './pages/Admin/CreateCategory';
+import CreateProduct from './pages/Admin/CreateProduct';
+import CreateBrand from './pages/Admin/CreateBrand';
+import Users from './pages/Admin/Users';
+import LoanDetails from './pages/Admin/LoanDetails';
+import Orders from './pages/user/Orders';
+import Profile from './pages/user/Profile';
+import Loans from './pages/user/Loans';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="user" element={<Dashboard />} />
+          <Route path="user/orders" element={<Orders />} />
+          <Route path="user/profile" element={<Profile />} />
+          <Route path="user/loans" element={<Loans />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/create-brand" element={<CreateBrand />} />
+          <Route path="admin/users" element={<Users />} />
+          <Route path="admin/loan-details" element={<LoanDetails />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
