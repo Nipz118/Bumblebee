28bf6567285038434aa0f655ccf5ecee8f4f27a7
Nipuni Perera <93609296+Nipz118@users.noreply.github.com>
2023-04-09 11:20:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+
+import cors from 'cors';
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+//routes
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoutes);
+
+//rst api
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to bumblebee app</h1>');
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
